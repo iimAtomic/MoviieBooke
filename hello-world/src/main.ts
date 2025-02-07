220b45537c0f37d2cc72cb41340c6ab612559082
Lux VEGBA <vegbahermerite@gmail.com>
2025-02-07 15:20:38 +0100

    add port

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -1,6 +1,11 @@
+/* eslint-disable */
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
